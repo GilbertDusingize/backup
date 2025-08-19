79d0576560df6d55352c852ce4f167e3bd07722e
gilbertdusingize <gilbertdusingize@gmail.com>
2025-08-19 08:05:23 +0200

    complete clone coffee web

---
+++ coffee/coffee.js
@@ -0,0 +1,49 @@
+const navlinks=document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton=document.querySelector("#menu-open-button");
+const menuCloseButton=document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", ()=>{
+//toggle mobile menu visibility
+document.body.classList.toggle("show-mobile-menu");
+});
+//close mobile menu
+menuCloseButton.addEventListener("click",()=>menuOpenButton.click());
+navlinks.forEach(link=>{link.addEventListener("click",()=>{
+  menuOpenButton.click()
+
+})});
+
+
+//Initialize Swiper
+const swiper = new Swiper('.carousel', {
+    loop: true,
+    grabCursor:true,
+    spaceBetween:25,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  //responsive navigation
+  breakpoints: {
+    0: {
+        slidesPerView:1
+    },
+    768: {
+        slidesPerView:2
+    },
+    1024: {
+        slidesPerView:3
+    }
+
+  }
+});
